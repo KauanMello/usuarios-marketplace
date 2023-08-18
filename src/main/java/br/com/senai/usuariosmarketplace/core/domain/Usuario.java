4ccd6b94348e2d9c7ea2e564e36cdc80ccd112ef
KauanMello <kauanmellocancelier2005@gmail.com>
2023-08-17 22:05:06 -0300

    Finalização da camada de persistencia de usuários.

---
+++ src/main/java/br/com/senai/usuariosmarketplace/core/domain/Usuario.java
@@ -0,0 +1,58 @@
+package br.com.senai.usuariosmarketplace.core.domain;
+
+import java.util.Objects;
+
+public class Usuario {
+
+	private String login;
+	private String nome;
+	private String senha;
+
+	public Usuario(String login, String nome, String senha) {
+		this.login = login;
+		this.nome = nome;
+		this.senha = senha;
+	}
+	
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(login);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		return Objects.equals(login, other.login);
+	}
+	
+}
