4eb95d80ad7c6f2561a7a845a74007d058350fdd
KauanMello <kauanmellocancelier2005@gmail.com>
2023-08-24 22:18:56 -0300

    Inicio de uso do lombok.

--- src/main/java/br/com/senai/usuariosmarketplace/core/domain/Usuario.java
+++ src/main/java/br/com/senai/usuariosmarketplace/core/domain/Usuario.java
@@ -2,39 +2,23 @@ package br.com.senai.usuariosmarketplace.core.domain;
 
 import java.util.Objects;
 
-public class Usuario {
+import lombok.Getter;
+import lombok.Setter;
 
+public class Usuario {
+	
+	@Getter @Setter
 	private String login;
+	
+	@Getter @Setter
 	private String nome;
+	
+	@Getter @Setter
 	private String senha;
 
 	public Usuario(String login, String nome, String senha) {
 		this.login = login;
 		this.nome = nome;
-		this.senha = senha;
-	}
-	
-	public String getLogin() {
-		return login;
-	}
-
-	public void setLogin(String login) {
-		this.login = login;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
 		this.senha = senha;
 	}
 
